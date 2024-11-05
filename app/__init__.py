cf8aa03d6d79e4e06370050fcd57c9b9b3751735
Dan Pablo <danpablo@Dans-MacBook-Pro.local>
2024-11-05 22:07:31 +0800

    Added CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from flask_cors import CORS
 from app.models import BaseGoogleSheet, GoogleSheetsHandler
 
 from .config import Config
@@ -7,6 +8,13 @@ from .config import Config
 
 def create_app():
     app = Flask(__name__)
+    # Enable CORS for all routes
+    # CORS(app, resources={r"/api/*": {"origins": ["https://e-message.vercel.app/"]}})
+    CORS(app)
+
+    # Optionally, you can specify which origins are allowed
+    # CORS(app, resources={r"/api/*": {"origins": "https://your-allowed-origin.com"}})
+
     app.config.from_object(Config)
 
     from .routes import main
